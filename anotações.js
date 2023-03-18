71922dc0826fe21f324a2cc64b31d4edea7db6c3
Eliwelton Santos <weltonguita@yahoo.com.br>
2023-03-18 14:33:30 -0300

    Inicio do projeto

---
+++ anotações.js
@@ -0,0 +1,36 @@
+/**
+ * HTTP
+ * - Método
+ * - URL
+ * 
+ * GET, POST, PUT, PATCH, DELETE
+ * 
+ * GET => Buscar um recurso do back-end
+ * POST => Criar um recurso no back-end
+ * PUT => Atualizar um recurso no back-end
+ * PATCH => Atualizar uma informação especifica de um recurso no back-end
+ * DELETE => Deleta um recurso no back-end
+ *
+ * GET/users => Buscando usuários no back-end
+ * POST/users => Criar um usuário no back-end
+ * 
+ * StateFul - Stateless
+ *  - Stateful => Salva apenas em mémoria
+ *  - Stateless => Salva em bancos ou arquivos
+ * 
+ * cabeçalhos (Requisições/respostas) => Metadados 
+ * 
+ * HTTP state Code => ex. status 200, 201, 404 
+ * 
+ * Strems => Conseguimos trabalhos com dados sem presisar que eles tenham sido 
+ * carregados completamente ex.(NetFlix e spotify)
+ * 
+ * Tipos de streams
+ * - Readable - Streams de leitura (Ler os arquivos aos poucos)
+ * - Weritable - Streams de escritas (Escrever os aquivos os poucos)
+ * - Transform - Streams de transformação (Transforma um dado em outro)
+ * 
+ * Stdin => È uma string de entrada
+ * Stdout => è uma string de saída*/
+
+
---
+++ mytest.js
@@ -0,0 +1,7 @@
+import http from 'node:http'
+
+const server = http.createrServer((req,res) => {
+    
+})
+
+server.listen(1337)
